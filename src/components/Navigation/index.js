1f45bb5699626101d5497d8a5b9762d173c3c3f8
Michael Giddings <mikegiddings@gmail.com>
2021-01-08 17:00:33 -0500

    added Nav to header and styles

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -5,7 +5,7 @@ import { Navbar, NavLink } from "react-bootstrap";
 function Navigation() {
   return (
     <div>
-      <Navbar expand="lg" bg="dark">
+      <Navbar expand="lg" bg="info" sticky="top">
         <NavLink className="nav-link" to="/">
           <div class="text-light">
             <h4 class="nav-title-font">Michael Giddings</h4>
@@ -13,8 +13,23 @@ function Navigation() {
         </NavLink>
         <ul class="navbar-nav ml-auto navitem-indent">
           <li class="nav-item">
+            <NavLink to="/about">
+              <div class="nav-font text-light">About Me</div>
+            </NavLink>
+          </li>
+          <li class="nav-item">
             <NavLink to="/portfolio">
               <div class="nav-font text-light">Portfolio</div>
+            </NavLink>
+          </li>
+          <li class="nav-item">
+            <NavLink to="/contact">
+              <div class="nav-font text-light">Contact</div>
+            </NavLink>
+          </li>
+          <li class="nav-item">
+            <NavLink to="/resume">
+              <div class="nav-font text-light">Resume</div>
             </NavLink>
           </li>
         </ul>
